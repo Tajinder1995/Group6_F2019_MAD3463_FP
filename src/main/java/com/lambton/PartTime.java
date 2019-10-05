244d298acb8a6196a29204d13aed2faaddec4a67
komal2006 <55511982+komal2006@users.noreply.github.com>
2019-10-04 21:20:05 -0400

    [simran] PartTime_constructor

--- src/main/java/com/lambton/PartTime.java
+++ src/main/java/com/lambton/PartTime.java
@@ -3,7 +3,12 @@ package com.lambton;
 public class PartTime
 {
 
-   
+
         private float rate;
         private float hoursWorked;
+    public PartTime(String name, int age, float rate, float hoursWorked) {
+        super(name, age);
+        this.rate = rate;
+        this.hoursWorked = hoursWorked;
+    }
 }
