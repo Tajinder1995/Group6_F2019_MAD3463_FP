58c6c5ae8d5fc294aa81093a669101d57dfb8581
komal2006 <55511982+komal2006@users.noreply.github.com>
2019-10-04 21:09:39 -0400

    [simran] create getter setter of employee class

--- src/main/java/com/lambton/Employee.java
+++ src/main/java/com/lambton/Employee.java
@@ -4,6 +4,21 @@ public class Employee
 {
     private String name;
     private int age;
-    int a;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 
 }
